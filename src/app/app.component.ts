df0c39a28d667d1f6f372774c692eeb923daeb7b
birodani <birodani91@gmail.com>
2020-05-06 15:27:12 +0200

    add node-forge and bundle analyzer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { pki,util } from 'node-forge';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'modularization';
+export class AppComponent implements OnInit {
+  title = 'Hello encoded data!';
+
+  ngOnInit(): void {
+  this.test();
+  }
+
+  test(){
+    //'RSAES-PKCS1-V1_5' | 'RSA-OAEP' | 'RAW' | 'NONE' | null;
+    let rsa = pki.rsa;
+    var keypair = rsa.generateKeyPair({bits: 1024, e: 0x10001});
+    let cypher = keypair.publicKey.encrypt(this.title);
+    console.log(cypher);
+    console.log(util.encode64(cypher));
+    console.log(keypair.privateKey.decrypt(cypher));
+    //console.log(util.int32ToBytes(keypair.publicKey));
+  }
 }
